d21aace7ac50efd9d85e2eb1c9fd7ae484f0eed6
Pavel Polistovskiy <poliweb007@gmail.com>
2022-09-06 13:45:19 +0600

    Update page 404

---
+++ pages/404.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+  </script>
+
+<template>
+  <div
+    class="font-sans antialiased bg-white dark:bg-black text-black dark:text-white grid min-h-screen place-content-center overflow-hidden"
+    data-v-bc8852ab="">
+    <div class="fixed left-0 right-0 spotlight z-10" data-v-bc8852ab=""></div>
+    <div class="max-w-520px text-center z-20" data-v-bc8852ab="">
+      <h1 class="text-8xl sm:text-10xl font-medium mb-8" data-v-bc8852ab="">404</h1>
+      <p class="text-xl px-8 sm:px-0 sm:text-4xl font-light mb-16 leading-tight" data-v-bc8852ab="">Page not found</p>
+      <div class="w-full flex items-center justify-center" data-v-bc8852ab=""><NuxtLink to="/"
+          class="gradient-border text-md sm:text-xl py-2 px-4 sm:py-3 sm:px-6 cursor-pointer" data-v-bc8852ab="">Go back
+          home</NuxtLink></div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
