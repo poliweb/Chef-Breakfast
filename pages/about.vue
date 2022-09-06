860cd492afebc6387bb4442370b58110aca3a7a7
Pavel Polistovskiy <poliweb007@gmail.com>
2022-09-06 07:51:30 +0600

    Add and Update about.vue and favicon.ico

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,18 @@
+<script setup>
+  useHead({
+    title: 'About Chef Breakfast',
+    meta: [
+    { name: 'description', content: 'Site Chef Breakfast. Cafe website template. The template is developed on the Nuxt-3 framework' }
+  ],
+  })
+</script>
+
 <template>
   <div>
     <h1>About</h1>
   </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
 
 <style>
 
