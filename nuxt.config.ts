9b8e5afbe8da594d881e3a27331b623e33db740d
Pavel Polistovskiy <poliweb007@gmail.com>
2022-09-14 14:59:49 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icons'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
     css: ['animate.css', '~/assets/css/main.css']
 })
