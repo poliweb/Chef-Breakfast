ee07d3e00bd1dffbe4f8089b7e1b24e4b545198d
Pavel Polistovskiy <poliweb007@gmail.com>
2022-09-17 07:55:27 +0600

    Update app.vue, main.css, add blog/index.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Roboto:wght@100;300;400;500;700;900&display=swap'
+    }
+  ]
+})
+console.log('✋ Hi!  This Project from PoliWeb Developer. ⛔This project is not in production yet ')
+</script>
 <template>
   <div>
     <NuxtLayout>
---
+++ pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto my-24 px-5">
+    <h1 class="title">My Blog</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
