1d4bef756e089039f2133d8898d1a7577343761f
Pavel Polistovskiy <poliweb007@gmail.com>
2022-11-19 10:32:44 +0600

    New file components/About/Chef.vue and Update pages/about.vue

---
+++ components/About/Chef.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container mx-auto my-24 px-5 text-gray-900">
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8">
+            <div class="w-full md:w-1/2 relative max-w-2xl">
+                <div class="pr-5 pb-5 sm:pr-10 sm:pb-10 md:pr-20 md:pb-20  z-20">
+                    <nuxt-img
+                        src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_face,w_592/v1668830017/CHEF%20BREAKFAST/Chef/StarChef_Right.webp"
+                        alt="Star Chef" class="rounded z-20 -hue-rotate-60" width="592" height="888"
+                        loading="lazy" />
+                </div>
+                <div class="podloga w-full h-full rounded bg-pink-300 absolute bottom-0 right-0 -z-50"></div>
+            </div>
+            <div class="w-full md:w-1/2">
+                <h3 class="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2">Star Chef</h3>
+                <h2 class="title">Ivan Pipkin</h2>
+                <p class="py-2 text-gray-600">
+                    Ivan Pipkin - created a group of talents to explore new territories and consolidate his philosophy
+                    outside the universe. CHEF BREAKFAST is the result of our commitment to push the limits, explore,
+                    experiment with new exciting ingredients in quality, origin and taste.
+                </p>
+                <h4>Ivan Pipkin became the brand chef of the Universe Hotel in 2013.</h4>
+                <p class="py-2 text-gray-600">
+                    Prior to that, he managed to work on passenger liners in Europe and in restaurants in Asia - Le
+                    Bonaparte, Pushkin, as well as in the Grand Hotell and Petrushki Internetshinol hotels, was the chef
+                    of the prestigious Abra Kadabra restaurant in the historical palace of the same name in Switzerland
+                    and "Lapland" in Stockholm - one one of the first new Nordic cuisine restaurants in Sweden.
+                </p>
+                <p class="py-2 text-gray-600">
+                    Ivan completed internships at the legendary Noma in Copenhagen and Chez Dominique in Helsinki. In
+                    2015, Ivan Pipkin opened the CHEF BREAKFAST cafe in the building of the Universe Hotel. The concept
+                    of the cafe is modern Russian cuisine in the author's performance, seasonal products, vegetables,
+                    herbs and work with farmers.
+                </p>
+                <p class="py-2 text-gray-600">
+                    <span class="bg-pink-500 px-2 text-white">Cafe CHEF BREAKFAST</span> is innovative and elegant, respectful of the age-old traditions of the peoples
+                    and the "Universe".
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
